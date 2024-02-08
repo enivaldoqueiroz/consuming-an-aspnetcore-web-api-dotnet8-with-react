daf76a2c06d441545e243496aca8f651f064608f
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-02-07 21:49:45 -0300

    Criação do metodo RegisterUser

--- AlunosApi/Services/AuthenticateService.cs
+++ AlunosApi/Services/AuthenticateService.cs
@@ -5,10 +5,12 @@ namespace AlunosApi.Services
     public class AuthenticateService : IAuthenticateService
     {
         private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public AuthenticateService(SignInManager<IdentityUser> signInManager)
+        public AuthenticateService(SignInManager<IdentityUser> signInManager, UserManager<IdentityUser> userManager)
         {
             _signInManager = signInManager;
+            _userManager = userManager;
         }
 
         public async Task<bool> Authenticate(string email, string password)
@@ -21,6 +23,22 @@ namespace AlunosApi.Services
         public async Task Logout()
         {
             await _signInManager.SignOutAsync();
+        }
+
+        public async Task<bool> RegisterUser(string email, string password)
+        {
+            var appUser = new IdentityUser
+            {
+                UserName = email,
+                Email = email
+            };
+
+            var result = await _userManager.CreateAsync(appUser, password);
+
+            if (result.Succeeded)
+                await _signInManager.SignInAsync(appUser, isPersistent: false);
+
+            return result.Succeeded;
         }
     }
 }
--- AlunosApi/Services/IAuthenticateService.cs
+++ AlunosApi/Services/IAuthenticateService.cs
@@ -3,6 +3,7 @@
     public interface IAuthenticateService
     {
         Task<bool> Authenticate(string email, string password);
+        Task<bool> RegisterUser(string email, string password);
         Task Logout();
     }
 }

db3ec0b1995148e9d9c84822f1bd8baf1851738b
enivaldoqueiroz <enivaldo.jecq@gmail.com>
2024-02-11 11:23:31 +0000

    Implementação da logica de Incluir novo aluno ou Atualizar o aluno

--- clientereact/src/pages/NovoAluno/index.js
+++ clientereact/src/pages/NovoAluno/index.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './styles.css';
 
 import { FiCornerDownLeft, FiUserPlus } from 'react-icons/fi'
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 
 export default function NovoAluno() {
+
+    var {alunoId} = useParams();
+    
     return (
         <div className='novo-aluno-container'>
             <div className='content'>
                 <section className='form'>
                     <FiUserPlus size={105} color='#17202a' />
-                    <h1>Text</h1>
+                    <h1>{alunoId === '0' ? 'Incluir Novo Aluno' : 'Atualizar Aluno'}</h1>
                     <Link className='back-link' to='/alunos'>
                         <FiCornerDownLeft size={25} color='#17202a' />
                         Retornar
@@ -20,7 +23,9 @@ export default function NovoAluno() {
                     <input placeholder='Nome'></input>
                     <input placeholder='Email'></input>
                     <input placeholder='Idade'></input>
-                    <button className='button' type='submit'>Texto</button>
+                    <button className='button' type='submit'>
+                        {alunoId === '0' ? 'Incluir' : 'Atualizar'}
+                    </button>
                 </form>
             </div>
         </div>
